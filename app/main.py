84b79ba513d0f47925f606f0349dd72a72ca9414
Nicolas Oulianov <oulianov>
2022-10-23 13:30:49 +0200

    Adds placeholder interface

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from deta import Deta
 
-st.markdown("# Hello world !")
+# Load database
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("mates")
+
+st.markdown("# Meet mates")
+
+with st.form("form"):
+    name = st.text_input("Your full name", placeholder="Lowis Douglas")
+    location_name = st.text_input(
+        "Public transport station close to your home", placeholder="Eg: Châtelet"
+    )
+    st.form_submit_button("Search mates")
+    st.markdown(
+        "**By clicking on search mate, you agree to share the above info with other users.**"
+    )
