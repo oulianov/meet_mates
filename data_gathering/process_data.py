35d6da21c05ab656b2c657e5226c27f95bfc8601
Nicolas Oulianov <oulianov>
2022-10-23 16:54:09 +0200

    Remove duplicates

--- data_gathering/process_data.py
+++ data_gathering/process_data.py
@@ -11,6 +11,7 @@ lines = pd.read_csv("data/fr-idf_ntfs/lines.txt")
 # %%
 
 slim_stops = stops[["stop_name", "stop_lon", "stop_lat"]]
+slim_stops.drop_duplicates(subset=["stop_name"], inplace=True)
 # %%
 slim_stops.to_csv("output_data/slim_stops.csv")
 # %%
