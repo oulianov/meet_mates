8b15749d854853493642610186795daa8b1baf23
Nicolas Oulianov <oulianov>
2022-10-23 17:35:41 +0200

    Handle case where not enough mates

--- app/main.py
+++ app/main.py
@@ -36,7 +36,8 @@ def find_mates(coords: Dict[str, float], min_mates=5) -> pd.DataFrame:
     # Compute a proper geodesic otherwise.
     mates_distances = np.linalg.norm((mates_position - coords_vec), ord=1, axis=1)
     # Select all top 5 closest mates
-    min_indices = np.argpartition(mates_distances, min_mates - 1)[:min_mates]
+    n = min(min_mates, all_mates.shape[0])
+    min_indices = np.argpartition(mates_distances, n - 1)[:n]
     all_mates_df = all_mates_df.iloc[min_indices]
     # Compute proper meter distance for them
     all_mates_df["distance (km)"] = all_mates_df.apply(
